a63d70983e4cc1c7998f0048d52ef62ea6bab970
Tegridy <jarjak4@gmail.com>
2021-03-08 19:54:23 +0100

    Added navbar responsivness

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  showMenu = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  toggleMenu(): void {
+    this.showMenu = !this.showMenu;
+  }
 }
