bc57165a1e261befc6633731277854fcb72719d0
Tegridy <jarjak4@gmail.com>
2021-03-30 16:26:15 +0200

    Added home page and fixed navbar backdrop issue

---
+++ src/app/home/recommended-products/recommended-products.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pmp-recommended-products',
+  templateUrl: './recommended-products.component.html',
+  styleUrls: ['./recommended-products.component.scss']
+})
+export class RecommendedProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
