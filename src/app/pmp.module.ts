1168ac924424a53dd6aa018bd1a2bb6b58f84d20
Tegridy <jarjak4@gmail.com>
2021-04-08 18:17:47 +0200

    Minor silder fixes

--- src/app/pmp.module.ts
+++ src/app/pmp.module.ts
@@ -6,13 +6,15 @@ import { PmpComponent } from './pmp.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { RecommendedProductsComponent } from './home/recommended-products/recommended-products.component';
+import { ProductSmallComponent } from './product-small/product-small.component';
 
 @NgModule({
   declarations: [
     PmpComponent,
     NavbarComponent,
     HomeComponent,
-    RecommendedProductsComponent
+    RecommendedProductsComponent,
+    ProductSmallComponent
   ],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   imports: [
---
+++ src/app/product-small/product-small.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pmp-product-small',
+  templateUrl: './product-small.component.html',
+  styleUrls: ['./product-small.component.scss']
+})
+export class ProductSmallComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
