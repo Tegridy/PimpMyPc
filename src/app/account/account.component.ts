a3723c1f29cd199700e2b80aeb95939703b6f7ed
Tegridy <jarjak4@gmail.com>
2022-02-06 12:42:48 +0100

    Fix account component ui

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -3,16 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'pmp-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.scss']
+  styleUrls: [],
 })
 export class AccountComponent implements OnInit {
-
   showAccountMenu = false;
+  username = 'User';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.username = sessionStorage.getItem('username') as string;
   }
+
+  ngOnInit(): void {}
 
   toggleAccountMenu(): void {
     this.showAccountMenu = !this.showAccountMenu;
--- src/app/account/my-orders/my-orders.component.ts
+++ src/app/account/my-orders/my-orders.component.ts
@@ -5,7 +5,7 @@ import { SingleOrder } from '../../shared/model/Order';
 @Component({
   selector: 'pmp-my-orders',
   templateUrl: './my-orders.component.html',
-  styleUrls: ['./my-orders.component.scss'],
+  styleUrls: [],
 })
 export class MyOrdersComponent implements OnInit {
   userOrders: SingleOrder[] = [];
